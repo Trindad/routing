69967199443cf95756e8ddbf4d8f46c6647839f2
Maurício André Cinelli <mauricio.andre.cinelli@gmail.com>
2015-03-15 19:30:51 -0700

    Adicionado arquivos de teste e adicionado funções de leitura dos arquivos e criação do grafo a partir das mesmas

--- main.cpp
+++ main.cpp
@@ -15,6 +15,10 @@
 #include <stdexcept> // for std::runtime_error
 #include <vector>
 #include <random>
+#include <cstdlib>
+#include <string>
+#include <fstream>
+#include <sstream>
 
 using namespace std;
 
@@ -48,6 +52,104 @@ typedef struct _t
 }Traffic;
 
 static vector< int > parent; 
+
+void help()
+{
+
+}
+
+Graph *readGraph(string _filename)
+{
+  Graph *graph = new Graph;
+
+  ifstream file (_filename);
+  string line;
+  int nNodes = 0;
+
+  if (file.good())
+  {
+    int lineCount = 0;
+
+    while ( getline (file,line) )
+    {
+      if (lineCount == 0) {
+        stringstream s(line);
+        s >> nNodes;
+        nNodes++;
+        graph->numberOfNodes = nNodes;
+        graph->edges = new Edge*[nNodes];
+        
+        for (int i = 0; i < nNodes; i++) {
+          graph->edges[i] = new Edge[nNodes];
+
+          for (int j = 0; j < nNodes; j++) {
+            graph->edges[i][j].target = 0;
+          }
+        }
+
+        graph->degree = vector<int>(nNodes, 0);
+
+        lineCount++;
+        continue;
+      }
+
+      stringstream ss(line); // Insert the string into a stream
+      int source;
+      int target;
+
+      ss >> source >> target;
+
+      cout << source << " " << target << endl;
+
+      graph->degree[source] += 1;
+
+      Edge e;
+      e.target = target;
+      e.weight = 1;
+      e.capacity = 0;
+      e.next = NULL;
+
+      graph->edges[source][graph->degree[source]] = e;
+
+      lineCount++;
+    }
+    file.close();
+  } else {
+    printf("Could not open graph file.\n");
+    exit(EXIT_FAILURE);
+  }
+
+  return graph;
+}
+
+vector< vector<int> > readTrafficMatrix(int nodes, string _filename)
+{
+  vector< vector<int> > matrix = vector< vector<int> >(nodes, vector<int>(nodes, -1));
+
+  ifstream file (_filename);
+  string line;
+
+  if (file.good())
+  {
+    while ( getline (file,line) )
+    {
+      stringstream ss(line); // Insert the string into a stream
+      unsigned int source;
+      unsigned int target;
+      int traffic;
+
+      ss >> source >> target >> traffic;
+
+      matrix[source][target] = traffic;
+    }
+    file.close();
+  } else {
+    printf("Could not open traffic file.\n");
+    exit(EXIT_FAILURE);
+  }
+
+  return matrix;
+}
 
 /**
  * Implementação para encontrar um caminho mínimo de u até v
@@ -112,8 +214,34 @@ void minimumCostFlow(Graph graph, vector< vector<int> > traffic)
 
 }
 
-int main(void)
+int main(int argc, const char * argv[])
 {
-	
-	return 0;
+  if(argc <= 2)
+  {
+    help();
+    return EXIT_FAILURE;
+  }
+
+  string arquivoTrafego = argv[2];
+
+  Graph *graph = readGraph(string(argv[1]));
+  vector< vector<int> > trafficMatrix = readTrafficMatrix(graph->numberOfNodes, string(argv[2]));
+
+  for (int i = 1; i < graph->numberOfNodes; i++) {
+    printf("Node %d: ", i);
+    for (int j = 1; j < graph->numberOfNodes; j++) {
+      printf("%d", graph->edges[i][j].target);
+    }
+
+    printf("\n---------------\n");
+  }
+
+  // for (int i = 1; i < graph->numberOfNodes; i++) {
+  //   for (int j = 1; j < graph->numberOfNodes; j++) {
+  //     printf("%d ", trafficMatrix[i][j]);
+  //   }
+  //   printf("\n");
+  // }
+
+  return EXIT_SUCCESS;
 }
